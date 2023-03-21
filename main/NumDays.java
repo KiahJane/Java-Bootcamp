3beff265c6d71da667fb0af8349faaf26fa117d0
Kiah Jane Jones <62849361+KiahJane@users.noreply.github.com>
2023-03-21 09:22:06 +0100

    final commit for 4th task for Java Bootcamp (13.03.2023)

--- main/NumDays.java
+++ main/NumDays.java
@@ -5,46 +5,56 @@ import java.util.Scanner;
 public class NumDays {
 
     public static Integer askNums() {
-        /* helper method -- asks & validates user input, saves array of integers until negative integer entered */
+        /* helper method -- asks & validates user input, returns integer entered */
         Scanner sc = new Scanner(System.in);
-        Integer sum = 0;
-        String test = "";
-        boolean shouldRun = true;
+        String test;
+        int num;
 
         do {
             try {
                 System.out.print("Please enter an integer:");
                 test = sc.nextLine();
-                int num = Integer.parseInt(test);
+                num = Integer.parseInt(test);
 
-                if (num > 0) {
-                    String weekDay = WeekDayUtil.getWeekDayByUserInput(num);
-                    System.out.println("You have entered weekday " + weekDay);
-                }
-                if (num > 0 && num % 2 == 0) {
-                    sum += num;
-                }
-                if (num == 0) {
-                    continue;
-                }
                 if (num < 0) {
-                    shouldRun = false; // this will stop the iteration when a negative integer is received
+                    break;
+                } else {
+                    return num;
                 }
-
-                return sum;
             } catch (Exception ex) {
-                System.out.println();
+                System.out.println("You have entered an invalid character");
             }
-        } while (shouldRun);
-
-        return null;
+        } while (true);
+        return num;
     }
 
-    public static void main(String[] args) {
-        NumDays numDays = new NumDays();
+    public static int getDay() {
+        int num = askNums();
+        int sum = 0;
 
-        int sum = askNums();
+        while(num >= 0){
+            if(num == 0){
+                num = askNums();
+            }
+            String weekDay = WeekDayUtil.getWeekDayByUserInput(num);
+            System.out.println("You have entered weekday " + weekDay);
+            if(num % 2 == 0){
+                sum += num;
+            }
+            num = askNums();
 
+            if (num < 0) {
+                System.out.println("You have exited by entering a negative number");
+                break;
+            }
+        }
+        return sum;
+    }
+
+    public static void main (String[] args){
+
+        int sum = getDay();
         System.out.println("The sum of all entered even numbers is: " + sum);
     }
+
 }
